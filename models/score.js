cbf2387dc41219a164a48d15166a6717e4b04a7e
zakrnem <zakrnem@zohomail.com>
2024-04-20 16:08:55 -0600

    Rename leaderboard model to score

--- models/score.js
+++ models/score.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const leaderboardSchema = new Schema({
+const scoreSchema = new Schema({
   gameboard: { type: Schema.Types.ObjectId, ref: "Gameboard", required: true },
-  name: { type: String, required: true, minLength: 4, maxLength: 18 },
+  user: { type: String, required: true, minLength: 4, maxLength: 18 },
   score: { type: Schema.Types.Number, min: 0, required: true },
   date: { type: Date, default: Date.now },
 });
 
-export default mongoose.model("Leaderboard", leaderboardSchema);
+export default mongoose.model("Score", scoreSchema);
