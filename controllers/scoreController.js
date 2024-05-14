f01eaffc1c658ab3ad80398ccebb24494dcc384b
zakrnem <zakrnem@zohomail.com>
2024-05-13 18:46:01 -0600

    Implement formated date when user fetches scores

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -1,12 +1,22 @@
 import asyncHandler from "express-async-handler";
 import Score from "../models/score";
 import mongoose from "mongoose";
+import { format } from "date-fns";
 
 const scores_get = asyncHandler(async (req, res) => {
   try {
     const scores = await Score.find({ gameboard: req.params.id }).sort({
       seconds: -1,
     });
+    for (const key of Object.keys(scores)) {
+      const parsedDate = new Date(scores[key].date);
+      const date = format(parsedDate, "MM-dd-yyyy");
+      scores[key] = {
+        user: scores[key].user,
+        date,
+        seconds: scores[key].seconds,
+      };
+    }
     res.status(200).json(scores);
   } catch (error) {
     res
